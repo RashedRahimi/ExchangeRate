86f7771a36fe3db2d66326a61620f79dd7f17f5e
Rashed Rahimi <rashedrahimi01@gmail.com>
2023-08-09 12:25:59 +0430

    first phase of project

--- screens/change-rate-screen.tsx
+++ screens/change-rate-screen.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {Text, View} from 'react-native';
 const ChangeRateScreen = () => {
   return (
-    <View>
-      <Text>Change Rate screen</Text>
+    <View style={{alignItems: 'center', alignContent: 'center', flex: 1}}>
+      <Text style={{alignSelf: 'center', fontSize: 24, marginTop: 300}}>
+        Coming soon....!
+      </Text>
     </View>
   );
 };
